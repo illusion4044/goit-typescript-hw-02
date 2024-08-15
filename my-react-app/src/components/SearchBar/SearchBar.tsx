6f8f6d42e7be794d94d21a8ef283264c1fe9547e
Andrii <bugaychuk9aa@gmail.com>
2024-08-15 15:12:48 +0300

    general update 2.0

---
+++ my-react-app/src/components/SearchBar/SearchBar.tsx
@@ -0,0 +1,34 @@
+import { Field, Form, Formik } from 'formik';
+import toast from 'react-hot-toast';
+import css from './SearchBar.module.css';
+
+interface SearchBarProps {
+  onSearch: (query: string) => void;
+}
+
+export default function SearchBar({ onSearch }: SearchBarProps) {
+  return (
+    <Formik
+      initialValues={{ topic: '' }}
+      onSubmit={(values, actions) => {
+        if (!values.topic.trim()) {
+          toast.error('Please enter a search term');
+        } else {
+          onSearch(values.topic);
+          actions.resetForm();
+        }
+      }}
+    >
+      <Form className={css.searchBar}>
+        <Field
+          type="text"
+          name="topic"
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images "
+        />
+        <button type="submit">Search</button>
+      </Form>
+    </Formik>
+  );
+}
